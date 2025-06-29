402873edd8001666cca4187d727171400de7a897
Faizzz12 <89962846+Faizzz12@users.noreply.github.com>
2025-06-29 12:57:54 +0700

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://deploy-production-d54a.up.railway.app/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function getProfile() {
   try {
